06d957b65e4393c370d542daa996559e8827ad06
offici5l <offici5l@protonmail.com>
2025-09-04 21:23:37 +0100

    .

--- test/fast_extractor.py
+++ test/fast_extractor.py
@@ -11,7 +11,10 @@ def main(url, file_to_extract):
     os.makedirs("./output", exist_ok=True)
 
     try:
-        with RemoteZip(url) as z:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+        with RemoteZip(url, headers=headers) as z:
             filenames = [f.filename for f in z.infolist()]
             
             target_img = f"{file_to_extract}.img"
